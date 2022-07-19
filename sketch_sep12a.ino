5367d02df2080efb05a3bd08b9cd07ef6349f2a7
Darshit Desai <36150235+dmdarrow4597@users.noreply.github.com>
2022-07-19 19:44:45 +0530

    Add files via upload

---
+++ sketch_sep12a.ino
@@ -0,0 +1,12 @@
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(13,OUTPUT);// put your setup code here, to run once:
+}
+void loop() 
+{
+ digitalWrite(13,HIGH);
+ delay(10000);
+ digitalWrite(13,LOW);
+ delay(20000);// put your main code here, to run repeatedly:
+}
