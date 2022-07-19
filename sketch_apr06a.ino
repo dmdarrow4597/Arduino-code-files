8334e34512e3eb0944355edf6a4ea424fba2343b
Darshit Desai <36150235+dmdarrow4597@users.noreply.github.com>
2022-07-19 19:50:35 +0530

    Add files via upload

---
+++ sketch_apr06a.ino
@@ -0,0 +1,18 @@
+void setup()
+{
+  pinMode(A4, INPUT);
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);// put your setup code here, to run once:
+}
+float val = 0,valu;
+void loop()
+{
+  val = analogRead(A4);
+  val=(val/80);
+  Serial.println(val); // put your main code here, to run repeatedly:
+  digitalWrite(13, HIGH);
+  delay(val);
+  valu=12.8-val;
+  digitalWrite(13, LOW);
+  delay(valu);
+}
